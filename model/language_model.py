4eb27614034c55b771ebc898eedd4150cf6b387d
konsgavriil <konsgavriil@gmail.com>
2024-01-16 10:53:36 +0000

    Initial Commit

---
+++ model/language_model.py
@@ -0,0 +1,12 @@
+from transformers import AutoModelForSeq2SeqLM
+from peft import get_peft_config, get_peft_model, LoraConfig, TaskType
+model_name_or_path = "meta-llama/Llama-2-7b"
+tokenizer_name_or_path = "meta-llama/Llama-2-7b"
+
+peft_config = LoraConfig(
+    task_type=TaskType.SEQ_2_SEQ_LM, inference_mode=False, r=8, lora_alpha=32, lora_dropout=0.1
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path)
+model = get_peft_model(model, peft_config)
+model.print_trainable_parameters()
